58040c808baa5376e74346dd1e34810c71db85e5
DESKTOP-S6N028H\Administrator <tanbi2010@gmail.com>
2025-12-04 16:43:35 +0900

    update more exercise

--- PRACTICE/testing_system_assignment_7.sql
+++ PRACTICE/testing_system_assignment_7.sql
@@ -23,13 +23,15 @@ AccountID 				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 Email					VARCHAR(150) UNIQUE KEY NOT NULL,
 Username  				NVARCHAR(50) UNIQUE KEY NOT NULL,
 FullName  				NVARCHAR(150) NOT NULL,
-DepartmentID 			TINYINT UNSIGNED NOT NULL,
+DepartmentID 			TINYINT UNSIGNED,
 PositionID				TINYINT UNSIGNED NOT NULL,
 CreateDate				DATETIME DEFAULT NOW(),
 
 FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID) ON DELETE CASCADE,
 FOREIGN KEY (PositionID)	REFERENCES `Position`(PositionID) ON DELETE CASCADE
 );
+ALTER TABLE `Account`;
+
 
 -- create Table 4 group
 DROP TABLE IF EXISTS `Group`;
@@ -351,44 +353,7 @@ DROP TRIGGER IF EXISTS trg_bf_createDate;
 -- 					('VTI Sale 04', '1', '2018-04-10 00:00:00');
 
 
-
-
-SELECT * FROM Department;
-SELECT * FROM Position;
-SELECT * FROM `Account`;
-SELECT * FROM `Group`;
-SELECT * FROM `GroupAccount`;
-SELECT * FROM TypeQuestion;
-SELECT * FROM CategoryQuestion;
-SELECT * FROM Question;
-SELECT * FROM Answer;
-SELECT * FROM Exam;
-SELECT * FROM ExamQuestion;
-
--- Tạo trigger Không cho phép người dùng thêm bất kỳ user nào vào department "Sale" nữa, khi thêm thì hiện ra thông báo "Department "Sale" cannot add more user" 
-DROP TRIGGER IF EXISTS trg_NoAcceptDptSale;
-DELIMITER $$
-CREATE TRIGGER trg_NoAcceptDptSale
-BEFORE INSERT ON `Account`
-FOR EACH ROW
-BEGIN
-
--- WITH cte_departmentName AS(
--- SELECT DepartmentName INTO v_dept FROM Department WHERE DepartmentID = 1
--- )
-DECLARE v_dept VARCHAR(150);
-SELECT DepartmentName INTO v_dept FROM `Account` a
-JOIN  Department d ON d.DepartmentID=a.DepartmentID
-WHERE a.DepartmentID=1;
-IF  (NEW.DepartmentID= v_dept) THEN
-SIGNAL SQLSTATE '12345'
-SET MESSAGE_TEXT  = 'Department "Sale" cannot add more user';
-END IF;
-END $$
-DELIMITER ;
-INSERT INTO `testing_system_assignment_7`.`Account` (`Email`, `Username`, `FullName`, `DepartmentID`, `PositionID`, `CreateDate`) 
-VALUES ('Email11@gmail.com', 'Username11', 'Fullname11', '1', '2', '2020-04-09 00:00:00');
-
+-- Question 2: Tạo trigger Không cho phép người dùng thêm bất kỳ user nào vào department "Sale" nữa, khi thêm thì hiện ra thông báo "Department "Sale" cannot add more user" 
 DROP TRIGGER IF EXISTS trg_NoAcceptDptSale;
 DELIMITER $$   
 CREATE TRIGGER trg_NoAcceptDptSale
@@ -408,3 +373,219 @@ DELIMITER ;
 INSERT INTO `testing_system_assignment_7`.`Account` (`Email`, `Username`, `FullName`, `DepartmentID`, `PositionID`, `CreateDate`) 
 VALUES ('Email12@gmail.com', 'Username12', 'Fullname11', '1', '2', '2020-04-09 00:00:00');
 
+-- Question 3: Cấu hình 1 group có nhiều nhất là 5 user 
+DROP TRIGGER IF EXISTS trg_max5Account;
+DELIMITER $$   
+CREATE TRIGGER trg_max5Account
+BEFORE INSERT ON `GroupAccount` 
+FOR EACH ROW
+BEGIN 
+DECLARE v_amount_acc TINYINT DEFAULT 0;
+SELECT count(*) INTO  v_amount_acc FROM `GroupAccount` WHERE GroupID =NEW.GroupID;
+IF (v_amount_acc > 5) THEN
+-- dừng chương trình
+SIGNAL SQLSTATE '12345'
+-- gửi thông báo
+SET MESSAGE_TEXT = 'Cant add more User';
+END IF;
+END $$ 
+DELIMITER ;
+INSERT INTO `GroupAccount` (`GroupID`, `AccountID`) VALUES ('1', '13');
+
+-- Question 4: Cấu hình 1 bài thi có nhiều nhất là 10 Question 
+DROP TRIGGER IF EXISTS trg_max5Question;
+DELIMITER $$   
+CREATE TRIGGER trg_max5Question
+BEFORE INSERT ON ExamQuestion 
+FOR EACH ROW
+BEGIN   
+DECLARE v_amountQuestion TINYINT DEFAULT 0;
+SELECT count(*) INTO v_amountQuestion FROM ExamQuestion WHERE ExamID= NEW.ExamID;
+IF (v_amountQuestion > 5) THEN
+-- dừng chương trình
+SIGNAL SQLSTATE '12345'
+-- gửi thông báo
+SET MESSAGE_TEXT = 'Cant add more Account';
+END IF;
+END $$ 
+DELIMITER ;
+INSERT INTO ExamQuestion (`ExamID`, `QuestionID`) VALUES ('1', '15');
+
+-- Question 5: Tạo trigger không cho phép người dùng xóa tài khoản có email là admin@gmail.com (đây là tài khoản admin, không cho phép user xóa),  
+-- còn lại các tài khoản khác thì sẽ cho phép xóa và sẽ xóa tất cả các thông tin liên quan tới user đó 
+DROP TRIGGER IF EXISTS trg_DlAdmin;
+DELIMITER $$   
+CREATE TRIGGER trg_DlAdmin
+BEFORE DELETE ON  `Account` 
+FOR EACH ROW
+BEGIN   
+DECLARE v_email VARCHAR(150);
+SET v_email='admin@gmail.com';
+IF (OLD.Email =v_email) THEN
+-- dừng chương trình
+SIGNAL SQLSTATE '12345'
+-- gửi thông báo
+SET MESSAGE_TEXT = 'Cant delete Account';
+END IF;
+END $$ 
+DELIMITER ;
+DELETE FROM `Account` WHERE Email='admin@gmail.com';
+
+-- Question 6: Không sử dụng cấu hình default cho field DepartmentID của table Account, hãy tạo trigger cho phép người dùng khi tạo account không điền  
+-- vào departmentID thì sẽ được phân vào phòng ban "waiting Department" 
+DROP TRIGGER IF EXISTS trg_bfInsertAccount;
+DELIMITER $$   
+CREATE TRIGGER trg_bfInsertAccount
+BEFORE INSERT ON  `Account` 
+FOR EACH ROW
+BEGIN 
+	DECLARE newDep TINYINT DEFAULT 0;
+	SELECT DepartmentID INTO newDep FROM Department WHERE DepartmentName LIKE 'Waiting Department';
+	IF (NEW.DepartmentID IS NULL) THEN 
+		-- SELECT DepartmentID FROM `Account` WHERE NEW.DepartmentID=newDep;
+		SET NEW.DepartmentID = newDep; 
+        END IF;
+	IF (NEW.DepartmentID =0) THEN
+		-- dừng chương trình
+		SIGNAL SQLSTATE '12345'
+		-- gửi thông báo
+		SET MESSAGE_TEXT = 'Waiting Department doesnt exist';
+	END IF;
+END $$ 
+DELIMITER ;
+INSERT INTO `testing_system_assignment_4`.`Account` (`Email`, `Username`, `FullName`, `PositionID`) 
+VALUES ('Thuy1234@gmail.com', 'Thuy1234', 'Nguyễn Hồng M', '2');
+
+DROP TRIGGER IF EXISTS trg_delExam;
+DELIMITER $$   
+CREATE TRIGGER trg_delExam
+BEFORE DELETE ON  Exam 
+FOR EACH ROW
+BEGIN 
+	DECLARE v_createDateExam DATETIME;
+    SET  v_createDateExam=date_sub(now(),INTERVAL 2 day);
+	IF (OLD.CreateDate > v_createDateExam) THEN 
+		-- dừng chương trình
+		SIGNAL SQLSTATE '12345'
+		-- gửi thông báo
+		SET MESSAGE_TEXT = 'Can not delete this record';
+	END IF;
+END $$ 
+DELIMITER ;
+DELETE FROM Exam WHERE ExamID=12;
+
+-- Question 8: Viết trigger sửa lại dữ liệu cho đúng:  
+-- Nếu người dùng nhập vào gender của account là nam, nữ, chưa xác định Thì sẽ đổi lại thành M, F, U cho giống với cấu hình ở database
+DROP TRIGGER IF EXISTS trg_changeGender;
+DELIMITER $$   
+CREATE TRIGGER trg_changeGender
+BEFORE UPDATE ON  `Account`
+FOR EACH ROW
+BEGIN 
+	IF (NEW.Gender= 'Nam') THEN 
+    SET EW.Gender= 'M';
+    ELSEIF (NEW.Gender= 'Nữ') THEN 
+    SET EW.Gender= 'F';
+    ELSEIF (NEW.Gender= 'chưa xác định') THEN 
+    SET EW.Gender= 'U';
+	END IF;
+END $$ 
+DELIMITER ;
+
+-- Question 7: Cấu hình 1 bài thi chỉ cho phép user tạo tối đa 4 answers cho mỗi question, trong đó có tối đa 2 đáp án đúng. 
+-- phân tích: 1) mỗi question có tối đa 4 câu trả lời, 2) mỗi câu hỏi (NEW.câu hỏi = OLD câu hỏi và new. câu trả lời = old đáp án)
+
+
+
+-- Question 10: Viết trigger chỉ cho phép người dùng chỉ được update, delete các question khi question đó chưa nằm trong exam nào 
+DROP TRIGGER IF EXISTS trg_updateQues;
+DELIMITER $$   
+CREATE TRIGGER trg_updateQues
+BEFORE UPDATE ON  Question 
+FOR EACH ROW
+BEGIN 
+	DECLARE v_countQues TINYINT DEFAULT 0;
+   SELECT count(*) INTO v_countQues FROM ExamQuestion WHERE QuestionID= NEW.QuestionID;
+	IF (v_countQues > 0) THEN 
+		-- dừng chương trình
+		SIGNAL SQLSTATE '12345'
+		-- gửi thông báo
+		SET MESSAGE_TEXT = 'Can not update this record';
+	END IF;
+END $$ 
+DELIMITER ;
+
+UPDATE `Question` 
+SET `Content` = 'Hỏi về C##' WHERE (`QuestionID` = '3') and (`CategoryID` = '9') and (`TypeID` = '2');
+-- cách 2: sử dụng IF EXISTS (SELECT 1 FROM .... WHERE ...=...) THEN
+DROP TRIGGER IF EXISTS trg_updateQues;
+DELIMITER $$   
+CREATE TRIGGER trg_updateQues
+BEFORE UPDATE ON  Question 
+FOR EACH ROW
+BEGIN 
+	IF EXISTS (SELECT 1 FROM ExamQuestion WHERE QuestionID= NEW.QuestionID) THEN 
+		-- dừng chương trình
+		SIGNAL SQLSTATE '12345'
+		-- gửi thông báo
+		SET MESSAGE_TEXT = 'Can not update this record';
+	END IF;
+END $$ 
+DELIMITER ;
+UPDATE Question SET Content ='Hỏi về C##' WHERE QuestionID=3;
+
+-- Trigger để không cho xóa:
+DROP TRIGGER IF EXISTS trg_beDelQues;
+DELIMITER $$   
+CREATE TRIGGER trg_beDelQues
+BEFORE DELETE ON  Question
+FOR EACH ROW
+BEGIN 
+	IF EXISTS (SELECT 1 FROM ExamQuestion WHERE QuestionID= OLD.QuestionID) THEN 
+    SIGNAL SQLSTATE '12345'
+    SET MESSAGE_TEXT ='Can delete this record';
+	END IF;
+END $$ 
+DELIMITER ;
+DELETE FROM Question WHERE QuestionID=3;
+
+-- Question 12: Lấy ra thông tin exam trong đó: Duration <= 30 thì sẽ đổi thành giá trị "Short time" 30 < Duration <= 60 thì sẽ đổi thành giá trị "Medium time" Duration > 60 thì sẽ đổi thành giá trị "Long time"
+SELECT * FROM Exam;
+SELECT e.ExamID, e.code, e.Title, 	CASE
+									WHEN Duration <=30 THEN 'Short time'
+									WHEN Duration <=60 THEN 'Medium time'
+									ELSE 'Long time'
+									END AS Duration,CreatorID,CreateDate
+									FROM Exam e;
+-- Question 13: Thống kê số account trong mỗi group và in ra thêm 1 column nữa có tên là the_number_user_amount và mang giá trị được quy định như sau:
+-- Nếu số lượng user trong group =< 5 thì sẽ có giá trị là few
+-- Nếu số lượng user trong group <= 20 và > 5 thì sẽ có giá trị là normal
+-- Nếu số lượng user trong group > 20 thì sẽ có giá trị là higher
+SELECT ga.GroupID, count(*) , 		CASE
+									WHEN count(*) <= 5 THEN 'Few'
+                                    WHEN count(*) <= 20 THEN 'Normal'
+                                    ELSE 'Higher'
+                                    END AS the_number_user_amount
+
+FROM `GroupAccount` ga GROUP BY ga.GroupID;
+
+-- Thống kê số mỗi phòng ban có bao nhiêu user, nếu phòng ban nào không có user thì sẽ thay đổi giá trị 0 thành "Không có User"
+SELECT d.DepartmentName,					CASE
+											WHEN count(*)= 0 THEN 'Không có User'
+                                            ELSE count(*)
+											END AS the_number_user_amount
+                                            FROM `Account` a 
+                                            JOIN Department d ON d.DepartmentID=a.DepartmentID
+                                            GROUP BY a.DepartmentID;
+                                            
+SELECT * FROM Department;
+SELECT * FROM Position;
+SELECT * FROM `Account`;
+SELECT * FROM `Group`;
+SELECT * FROM `GroupAccount`;
+SELECT * FROM TypeQuestion;
+SELECT * FROM CategoryQuestion;
+SELECT * FROM Question;
+SELECT * FROM Answer;
+SELECT * FROM Exam;
+SELECT * FROM ExamQuestion;
